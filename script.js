92a53ba5499a1a0a3b1ae9fb684214d4346efd09
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-06-10 14:31:02 +0530

    final animation done

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
-console.log("Welcome to LAZAREV!");
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+
+    // for tablet smooth
+    tablet: { smooth: true },
+
+    // for mobile
+    smartphone: { smooth: true },
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from("#page1", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 0.2,
+  });
+  tl.from("#page1", {
+    transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+    borderRadius: "150px",
+    duration: 2,
+    ease: "expo.out",
+  });
+  tl.from("nav", {
+    opacity: 0,
+    delay: -0.2,
+  });
+  tl.from("#page1 h1, #page1 p, #page1 div", {
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.2,
+  });
+}
 
 function navAnimation() {
   const nav = document.querySelector("nav");
@@ -58,9 +116,6 @@ function navAnimation() {
 }
 
 
-navAnimation();
-
-
 function page2Animation() {
   var rightElems = document.querySelectorAll(".right-elem");
 
@@ -80,15 +135,11 @@ function page2Animation() {
     elem.addEventListener("mousemove", function (dets) {
       gsap.to(elem.childNodes[3], {
         x: dets.x - elem.getBoundingClientRect().x - 40,
-        y: dets.y - elem.getBoundingClientRect().y -100,
+        y: dets.y - elem.getBoundingClientRect().y - 100,
       });
     });
   });
 }
-
-page2Animation();
-
-
 
 function page3VideoAnimation() {
   var page3Center = document.querySelector(".page3-center");
@@ -125,4 +176,29 @@ function page3VideoAnimation() {
   });
 }
 
+function page6Animations() {
+  gsap.from("#btm6-part2 h4", {
+    x: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#btm6-part2",
+      scroller: "#main",
+      // markers:true,
+      start: "top 80%",
+      end: "top 10%",
+      scrub: true,
+    },
+  });
+}
+
+locomotiveAnimation();
+
+navAnimation();
+
+page2Animation();
+
 page3VideoAnimation();
+
+page6Animations();
+
+loadingAnimation();
